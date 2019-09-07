79bef075a34d006af2e958efa9edefbf000e14a3
yjolivier <bouxouyao@gmail.com>
2019-09-07 13:42:33 +0000

    Creation and customization of the header.php, footer.php and index.php

---
+++ footer.php
@@ -0,0 +1,55 @@
+			<footer class="row">
+				<div class="footer-header col-lg-12 col-sm-12 col-md-12">
+					<div class="row footer-header-title">
+						<div class="footer-logo col-lg-5 col-md-5 col-sm-12">
+							<p>SITE WEB</p>
+						</div>
+						<div class="search-bar col-lg-7 col-md-7 col-sm-12">
+							<form id="formulaire" method="POST">
+									<input class="champdesaisir" type="text" name="motdepasse" placeholder="Recherche">
+									<input class="bouton" name="btn" type="submit" value="go" />
+							</form>
+						</div>
+					</div>
+				</div>
+				<hr width="90%" size="1" color="#CECECE">
+				<div class="footer-bloc-info row">
+					<div  class="descriptition col-lg-5 col-md-5 col-sm-12">
+						<p>
+							Our goal at Borrow Loan Company is to provide
+							access to personal loans and education loan, car
+							loan, home loan at insight competitive interest rates
+							lorem ipsums. We are the loan provider, you can use
+							our loan product.
+						</p>
+					</div>
+					<div class="footer-menu col-lg-4 col-md-4 col-sm-12">
+						<ul>
+							<li><a href="#">ACCUEIL</a></li>
+							<li><a href="#">PRESENTATION</a></li>
+							<li><a href="#">A PROPOS</a></li>
+							<li><a href="#">BL0G</a></li>
+							<li><a href="#">PROGRAMME</a></li>
+							<li><a href="#">NOUS CONTACTER</a></li>
+						</ul>
+					</div>
+					<div class="reseau-social col-lg-2 col-md-2 col-sm-12">
+						<a href="#"><i class="fab fa-facebook-f"></i></a>
+						<a href="#"><i class="fab fa-instagram"></i></a>
+						<a href="#"><i class="fab fa-twitter"></i></a>
+						<a href="#"><i class="fas fa-envelope-square"></i></a>
+					</div>
+				</div>
+				<div class="signature col-lg-12">
+					<p class="">©designed by Olivier Yao</p>
+				</div>
+			</footer>
+		</div>
+	<!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+	</body>
+</html>
+				
---
+++ header.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>SITE WEB</title>
+		<meta charset="utf-8">
+    	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	    <!-- Bootstrap CSS -->
+	    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	    <!--FONTS-->
+	    <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700%7CMerriweather:300,300i,400,400i,700,700i" rel="stylesheet">
+	    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+	    <script src="https://kit.fontawesome.com/d8e469504a.js"></script>
+	    <!--css-->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
+	</head>
+	<body class="corps">
+		<div class="container-fluid">
+			<header class="row sticky-top">
+				<div class="header-logo col-lg-4 col-md-12 col-sm-6 col-6">
+					<p>SITE WEB</p>
+				</div>
+				<div class="header-menu col-lg-8 col-md-12 col-sm-6 col-6">
+					<nav class="navbar navbar-expand-md navbar-white bg-white">
+						<button class="navbar-toggler" data-toggle="collapse" data-target="#collapse_target">
+							<span class="navbar-toggler-icon btn-menu">
+								<i class="fas fa-align-justify"></i>
+							</span>
+						</button>
+						<div class="collapse  navbar-collapse" id="collapse_target">
+						<ul class="navbar-nav">
+							<li class="nav-item">
+								<a class="nav-link" href="#">ACCUEIL</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">PRESENTATION</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">A PROPOS</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">ME CONTACTER</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">BLOG</a>
+							</li>
+							<li class="nav-item">
+								<a class="nav-link" href="#">PROGRAMME</a>
+							</li>
+						</ul>
+						</div>
+					</nav>
+				</div>
+			</header>
+			
