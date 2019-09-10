c744823c5a675fdab9fde43e93470d0e7e142c57
yjolivier <bouxouyao@gmail.com>
2019-09-10 00:26:26 +0000

    [IMPR] Detail page (single)

---
+++ single.php
@@ -0,0 +1,26 @@
+<?php include("header.php"); ?>
+			<section class="row single">
+				<div class="container block-archive-container">
+			        <div class="row archive-article">
+			        	<div class="card block-archiver col-lg-8">
+				            <div class="card-img">
+				            	<img src="img/img4.jpg" class="card-img-top" alt="...">
+				            </div>
+				            <div class="card-body">
+				                <p class="card-text">
+				                    <h5>EVENEMENT</h5>
+				          			<p>Discover flexible car financing options for new or used cars, and specialty vehicles nsequat fringi porta.</p>
+				                </p>
+				                <div class="btn-article">
+				                	<a href="#">SAVOIR PLUS</a>
+				                </div>
+				            </div>
+			            </div>
+			            <div class="col-lg-4 sidebar">
+			            	bonjour le monde
+			            </div>
+			        </div>
+				</div>
+			</section>
+<?php include("footer.php"); ?>
+				
